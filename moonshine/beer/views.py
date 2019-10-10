f907d1d05f92d338c7cdbc24cd849b1ba3f88a3a
lockwarr <denislav66@gmail.com>
2019-10-10 17:22:56 +0300

    Post view and changes to the db

--- moonshine/beer/views.py
+++ moonshine/beer/views.py
@@ -27,12 +27,50 @@ class BeerView(views.APIView):
                                 }
         """
         try:
-            beer = BeerModel.objects.get(id=pk)
+            beer = BeerModel.objects.get(id=pk) # pylint: disable=no-member
             beer_serializer = self.serializer_class(beer)
 
             return Response(
                 beer_serializer.data,
                 status=status.HTTP_200_OK)
+
+        except Exception as ex:
+
+            return Response(
+                {'message': str(ex)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+
+    def post(self, request, *args, **kwargs):
+        """
+            Method:             Post
+            Url:                /api/beer/
+            Request headers:
+                                {
+                                    "Content-Type": "application/json",
+                                    "Accept": "application/json",
+                                }
+            Request body:       {
+                                    name: "",
+                                    beertype: "",
+                                    description: "",
+                                }
+            Response:
+                                {
+                                    beer.object.dict
+                                }
+        """
+        try:
+            beer_serializer = self.serializer_class(data=request.data)
+            if beer_serializer.is_valid():
+                beer_serializer.save()
+                return Response(
+                    beer_serializer.data,
+                    status=status.HTTP_201_CREATED)
+
+            return Response(
+                    beer_serializer.data,
+                    status=status.HTTP_400_BAD_REQUEST)           
 
         except Exception as ex:
 
